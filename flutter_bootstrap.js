a25cda775232e3bb043a0cb3fbde40942c10def6
nadanabill <nadanabilhelmyy@gmail.com>
2026-09-15 21:05:41 +0300

    Deploy website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3809482440" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1728065942" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
